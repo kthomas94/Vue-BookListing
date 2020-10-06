da5fbd51b045735d54010d7a1f2b6aa0c19181cf
Khalipha Thomas <70702235+kthomas94@users.noreply.github.com>
2020-10-06 15:16:43 -0400

    3rd module done

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,7 @@
-<template>
+<template v-for="book in books">
   <div>
     <h1>{{ title }}</h1>
+    <li>{{ book.title }} : {{ book.author }}</li>
   </div>
 </template>
 
@@ -10,6 +11,11 @@ export default {
   data() {
     return {
       title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Meil Gailman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" },
+      ],
     };
   },
 };
@@ -19,5 +25,14 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
