1bc0588a3bf9d3aedab7d79765a2fe3f283fddb3
Khalipha Thomas <70702235+kthomas94@users.noreply.github.com>
2020-10-06 15:29:47 -0400

    finishing up module 4

---
+++ src/components/BookItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <li>{{ book.title }}:{{ book.author }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: {
+    book: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
